283a6ccc42e8c535128b7bb6c337bd158e5f0a45
Jacob Lujan <600022570@fjuhsd.org>
2021-02-26 23:07:04 +0000

    Changed number.java

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jacob Lujan
 
 
 //write the Number class
@@ -9,20 +9,61 @@
 //to guide you
 
 //header for Number class
+public class Number
+{
 
 //private instance variable (must be an Integer)
-
+  Integer num;
 //constructor
+  public Number()
+  {
+    num = 0;
+  }
 
-
+  public Number(Integer i)
+  {
+    num = i;
+  }
 //getter method
-
-
+  public Integer getNum()
+  {
+    return num;
+  }
 //setter method
-
+  public void setNum(Integer z)
+  {
+    num = z;
+  }
 //isOdd method
-
+  public boolean isOdd()
+  {
+    if(num % 2 == 1)
+    {
+      return true;
+    }
+    return false;
+  }
 //isPerfect method
-
+  public boolean isPerfect()
+  {
+    int r = 0;
+    for(int x = 1; x < num; x++)
+    {
+      if(num % x == 0)
+      {
+        r = x + r;
+      }
+    }
+    if(r == num)
+    {
+      return true;
+    }
+    return false;
+  }
 
 //toString
+  public String toString()
+  {
+    return "" + num;
+  }
+}
